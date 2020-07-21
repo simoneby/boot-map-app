96e54dea568c6c861f77e338fadd28cb4747239e
Ola Opheim <Ola.Opheim@klp.no>
2020-07-21 09:36:28 +0200

    first commit

---
+++ src/main/java/com/example/ELKdemo/Application.java
@@ -0,0 +1,20 @@
+package com.example.ELKdemo;
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
