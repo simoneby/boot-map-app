0bb9cec8fd12502b91efd8ecbebc74b0d07f6c86
klp-ola <68115410+klp-ola@users.noreply.github.com>
2020-07-21 13:28:15 +0200

    Update HelloController.java

--- src/main/java/com/example/ELKdemo/HelloController.java
+++ src/main/java/com/example/ELKdemo/HelloController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+    
+@RequestMapping("/hello")
+    public String index2() {
 
+        CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+        HttpGet request = new HttpGet("http://46.101.166.115:8080/add/user?name=ClauseMann&email=cluseman@mail.ers");
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            System.out.println("http://46.101.166.115:8080/add/user?name=ClauseMann&email=cluseman@mail.ers triggered");
+            System.out.println(response.getStatusLine().toString());
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+        }
+        return "Triggered";
     }
-//New comment2
 }
