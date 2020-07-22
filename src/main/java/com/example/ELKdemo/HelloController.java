2a8221e0c4ad548299f59b7a4036c4e2e9b263ba
Simone Bystadhagen <simone.bystadhagen@gmail.com>
2020-07-22 12:06:55 +0200

    Update HelloController.java

--- src/main/java/com/example/ELKdemo/HelloController.java
+++ src/main/java/com/example/ELKdemo/HelloController.java
@@ -21,9 +21,10 @@ public class HelloController {
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             System.out.println("http://46.101.166.115:8080/add/user?name=ClauseMann&email=cluseman@mail.ers triggered");
             System.out.println(response.getStatusLine().toString());
+            return "Claus was triggered and hopefully added to database";
         } catch (Exception e) {
             System.out.println("ERROR: " + e);
         }
-        return "Triggered";
+        return "Failed to add to database";
     }
 }
